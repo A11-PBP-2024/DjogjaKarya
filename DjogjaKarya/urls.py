9d44579dd61ee14d783f58e7a4e56c66d9fef14d
Mirza Athallah Salman <atar.haryanto@gmail.com>
2024-10-23 21:15:21 +0700

    fixing urls.py

--- DjogjaKarya/urls.py
+++ DjogjaKarya/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('article/', include('article.urls')),
-    path('auth/', include('authentication.urls'))
+    path('auth/', include('authentication.urls')),
+    path('product/',include('product.urls'))
 ]
