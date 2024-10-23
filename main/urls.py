f4648025073d3c13f088e8afed03bdf057de2007
Rama <rizkyramadhani.ixh@gmail.com>
2024-10-23 20:42:59 +0700

    First Commit Wishlist

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from main.views import show_main
-
+from wishlist.views import show_wishlist
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('wishlist/', show_wishlist, name='show_wishlist'),
 ]
