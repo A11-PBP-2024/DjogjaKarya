2fe0e2624ce484dc21e7a8c50db9d49192d54cd0
Mirza Athallah Salman <atar.haryanto@gmail.com>
2024-10-23 13:19:50 +0700

    fixing login page n register page

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
+
+def show_main(request):
+    context = {"user": request.user}
+    return render(request, "main.html", context)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('main:show_main')
+      else : 
+          messages.error(request, "Username atau Password Salah, Coba Ulangi Lagi!")
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
