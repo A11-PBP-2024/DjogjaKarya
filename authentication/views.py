3e157f14575346a42c9cf071c4f66c6f1f1a48ce
Mirza Athallah Salman <atar.haryanto@gmail.com>
2024-10-24 17:58:45 +0700

    register, login, logout

--- authentication/views.py
+++ authentication/views.py
@@ -17,20 +17,21 @@ def register(request):
     return render(request, 'register.html', context)
 
 def login_user(request):
-   if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
 
-      if form.is_valid():
+        if form.is_valid():
             user = form.get_user()
             login(request, user)
             return redirect('main:show_main')
-      else : 
-          messages.error(request, "Username atau Password Salah, Coba Ulangi Lagi!")
+        else:
+            messages.error(request, "Username atau Password Salah, Coba Ulangi Lagi!")
+            return render(request, 'login.html', {'form': form})
 
-   else:
-      form = AuthenticationForm(request)
-   context = {'form': form}
-   return render(request, 'login.html', context)
+    else:
+        form = AuthenticationForm(request)
+        context = {'form': form}
+        return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
