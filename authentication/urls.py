49be7387d93ceadde0940b288e59e43c33fe5474
khaynaura <146971893+khaynaura@users.noreply.github.com>
2024-12-13 15:24:47 +0700

    adding auth backend fluttr

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', views.logout_user, name='logout'),
+    path('login-view/', views.login_view, name = "login_view"),
+    path('register-view/', views.register_view, name = "register_view"),
 ]
--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from product.views import show_category, show_detail, filter_category_ajax, add_product,get_product, edit_product, delete_product, get_product_id, get_form_data
+from product.views import show_category, show_detail, filter_category_ajax, add_product,get_product, edit_product, delete_product, get_product_id, get_form_data, add_product_flutter, edit_product_flutter, delete_product_flutter
 
 app_name = 'product'
 
@@ -13,6 +13,9 @@ urlpatterns = [
     path('edit/<int:id>/', edit_product, name='edit_product'),  # URL untuk edit produk
     path('delete/<int:id>/', delete_product, name='delete_product'),  # URL untuk delete produk
     path('product/<int:id>/', get_product_id, name='get_product_id'),
-     path('get-form-data/', get_form_data, name='get_form_data'),
+    path('get-form-data/', get_form_data, name='get_form_data'),
+    path('add-product-flutter/', add_product_flutter, name='add_product_flutter'),
+    path('edit-product-flutter/<int:id>/', edit_product_flutter, name='edit_product_flutter'),
+    path('delete-product-flutter/<int:id>/', delete_product_flutter, name='delete_product_flutter'),
 ]
 
